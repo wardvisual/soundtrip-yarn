681747b4d41a661ed88324f1ab70ee5b9448b6b6
wardvisual <wardvisual@gmail.com>
2022-11-06 23:07:26 +0000

    chore: complete initial home view

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -10,10 +12,99 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-      child: Text('HomeWorks!!'),
-      )
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.topLeft,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * .5,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter, 
+                colors: [
+                  Colors.white.withOpacity(0.5),
+                  Colors.white.withOpacity(0.1),
+                  Colors.black.withOpacity(0)
+                ],
+              )
+            ),
+          ),
+          SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(height: 40),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Recently Played",
+                          style: Theme.of(context).textTheme.headline6
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.history),
+                            SizedBox(width: 20),
+                            Icon(Icons.settings),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    physics: BouncingScrollPhysics(),
+                    padding: EdgeInsets.all(20),
+                    child: Row(
+                      children: [
+                        AlbumCard(),
+                        SizedBox(width: 16),
+                        AlbumCard(),
+                        SizedBox(width: 16),
+                        AlbumCard(),
+                        SizedBox(width: 16),
+                        AlbumCard(),
+                        SizedBox(width: 16),
+                        AlbumCard(),
+                        SizedBox(width: 16),
+                        AlbumCard(),
+                      ],
+                    )
+                  )
+                ],
+              ),
+            )
+          )
+      ],)
+    );
+  }
+}
+
+class AlbumCard extends StatelessWidget {
+  const AlbumCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.asset(
+          "assets/beliver.jpg",
+          width: 125,
+          height: 125,
+        ),
+        SizedBox(height: 10),
+        Text("Beliver")
+      ],
     );
   }
 }
